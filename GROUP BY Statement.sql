d1b95151510a748bb005d85c343d074a9529a1d9
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 17:55:36 -0600

    Update GROUP BY Statement.sql

--- GROUP BY Statement.sql
+++ GROUP BY Statement.sql
@@ -17,3 +17,17 @@ GROUP BY gender;
 SELECT gender, MIN(age), MAX(age), COUNT(age),AVG(age)
 FROM employee_demographics
 GROUP BY gender;
+
+-- When using GROUP BY you must have the same columns you're SELECTing in the GROUP BY statement
+SELECT gender
+FROM employee_demographics
+GROUP BY gender;
+
+SELECT occupation
+FROM employee_salary
+GROUP BY occupation;
+
+SELECT first_name
+FROM employee_demographics
+GROUP BY gender;
+-- This will fail as the gender column isn't in the SELECT statement
