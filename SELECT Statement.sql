c8366e1137b7fb02149e96cee408e73f9b9e1dbd
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 16:38:37 -0600

    SELECT Statement Definition and Syntax

---
+++ SELECT Statement.sql
@@ -0,0 +1,9 @@
+-- SELECT STATEMENT
+
+-- The SELECT statement in SQL is used to retrieve data from one or more tables and what columns you want to in your ouput.
+-- We can also select a specific number of columns based on our requirement.
+
+-- BASIC SYNTAX
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
