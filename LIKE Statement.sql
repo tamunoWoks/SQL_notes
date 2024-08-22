5038a605978cc963ce170bddcae127f7544fa8d6
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 17:18:57 -0600

    Intro to LIKE Statement

---
+++ LIKE Statement.sql
@@ -0,0 +1,5 @@
+-- LIKE STATEMENT
+
+-- In SQL, LIKE is used in a WHERE clause to search for a specified pattern in a column. 
+-- You typically use it with wildcard characters % and _ to filter rows that match the pattern.
+
