3b271da6357b396aa43148d62a908b2c3b443259
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 17:48:47 -0600

    GROUP BY Intro and Syntax

--- GROUP BY Statement.sql
+++ GROUP BY Statement.sql
@@ -1,1 +1,10 @@
+-- GROUP BY
 
+-- The GROUP BY clause in SQL is used to group rows that have the same values in specified columns into summary rows, like aggregating data.
+-- It is commonly used with aggregate functions such as COUNT(), SUM(), MAX(), MIN(), and AVG() to perform operations on each group of rows.
+
+--SYNTAX
+SELECT column_name, aggregate_function(column_name)
+FROM table_name
+WHERE condition
+GROUP BY column_name;
