cb34df77de87821929938b529bd33dd772df0f55
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 17:19:54 -0600

    Update LIKE Statement.sql

--- LIKE Statement.sql
+++ LIKE Statement.sql
@@ -3,3 +3,5 @@
 -- In SQL, LIKE is used in a WHERE clause to search for a specified pattern in a column. 
 -- You typically use it with wildcard characters % and _ to filter rows that match the pattern.
 
+-- % (percent sign) means anything and can represent zero, one, or multiple characters.
+-- _ (underscore) represents a single specific character.
