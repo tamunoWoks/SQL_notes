b53da0028ea61bc530b9298743d15bd52046248e
Tamunosaki Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-22 16:50:12 -0600

    Using calculated fields

--- SELECT Statement.sql
+++ SELECT Statement.sql
@@ -19,3 +19,13 @@ FROM parks_and_recreation.employee_demographics;
 -- Selecting multiple specific columns:
 SELECT first_name, last_name
 FROM parks_and_recreation.employee_demographics;
+
+-- Using calculated fields:
+SELECT first_name, last_name, age, age +10
+FROM parks_and_recreation.employee_demographics;
+
+-- Math in SQL follows the PEMDAS order of operation 
+-- PEMDAS = Parenthesis, Exponent, Multiplication, Division, Addition, Subtraction.
+SELECT first_name, last_name, age, (age +10) * 2
+FROM parks_and_recreation.employee_demographics;
+
