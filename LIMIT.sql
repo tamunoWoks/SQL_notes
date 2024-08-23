4495b5f6be1be10f4a2a580a880524cc97c2dfc3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:19:57 -0600

    Description

--- LIMIT.sql
+++ LIMIT.sql
@@ -1,1 +1,4 @@
 -- LIMIT CLAUSE
+
+-- In SQL, the LIMIT clause is used to limit the number of rows returned by a query. 
+-- It is often useful when you want to retrieve only a specific subset of data from a larger dataset, such as the first 10 rows or 100 rows.
