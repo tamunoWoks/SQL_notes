2ba2ced81e2868daa4d07c94ded1b3116d2c2ae9
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 14:06:03 -0600

    Syntax

--- ORDER_BY.sql
+++ ORDER_BY.sql
@@ -2,3 +2,8 @@
 
 -- ORDER BY is a SQL clause used to sort the result set of a query by one or more columns, either in ascending (ASC) or descending (DESC) order.
 -- The ORDER BY keyword sorts the records in ascending order by default. To sort the records in descending order, use the DESC keyword.
+
+-- SYNTAX
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1 [ASC|DESC], column2 [ASC|DESC], ...;
