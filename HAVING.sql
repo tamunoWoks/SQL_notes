030fd1be0c496006b5371bf919c2763c19a5c43d
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:08:48 -0600

    Filter on the AVG age using WHERE

--- HAVING.sql
+++ HAVING.sql
@@ -11,3 +11,13 @@ FROM table_name
 WHERE condition
 GROUP BY column1, column2
 HAVING aggregate_condition;
+
+
+-- Examples
+-- Filter on the avg age using where
+SELECT gender, AVG(age)
+FROM employee_demographics
+WHERE AVG(age) > 40
+GROUP BY gender
+-- this doesn't work because of order of operations. On the backend Where comes before the group by. So you can't filter on data that hasn't been grouped yet
+-- this is why Having was created
