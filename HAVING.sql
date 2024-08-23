96c75d661a3b4250a8a3f4a075babd443e6de1b5
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:05:55 -0600

    HAVING syntax

--- HAVING.sql
+++ HAVING.sql
@@ -4,3 +4,10 @@
 -- It's similar to the WHERE clause, but WHERE filters rows before aggregation, while HAVING filters rows after aggregation.
 -- Where is going to filters rows based off columns of data
 -- Having is going to filter rows based off aggregated columns when grouped
+
+-- SYNTAX
+SELECT column1, column2, aggregate_function(column3)
+FROM table_name
+WHERE condition
+GROUP BY column1, column2
+HAVING aggregate_condition;
