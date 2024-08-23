b50d2512cdedfb311f690a85c11f78cb348caab0
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:47:44 -0600

    Description

--- ALIASING.sql
+++ ALIASING.sql
@@ -1,1 +1,5 @@
 -- ALIASING
+
+-- In SQL, aliasing refers to the process of giving a temporary name to a table or a column
+-- It makes the query more readable or handles situations where two or more columns have the same name. 
+-- Aliasing is typically done using the 'AS' keyword.
