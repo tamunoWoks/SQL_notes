858f4e3c3c990e06293a1d31724f360c7823fdd7
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:20:54 -0600

    LIMIT syntax

--- LIMIT.sql
+++ LIMIT.sql
@@ -2,3 +2,8 @@
 
 -- In SQL, the LIMIT clause is used to limit the number of rows returned by a query. 
 -- It is often useful when you want to retrieve only a specific subset of data from a larger dataset, such as the first 10 rows or 100 rows.
+
+-- SYNTAX
+SELECT column1, column2
+FROM table_name
+LIMIT number_of_rows;
