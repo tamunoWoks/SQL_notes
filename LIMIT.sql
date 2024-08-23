6e5403e616e8fef97576718d9078717b8de0bfa8
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:29:00 -0600

    Specify the starting place

--- LIMIT.sql
+++ LIMIT.sql
@@ -15,3 +15,12 @@ FROM employee_demographics
 ORDER BY first_name
 LIMIT 3;
 -- This query will return the first 3 rows from the employee_demographic table in by first name in ascending order.
+
+
+-- Specify the starting place
+-- There is an additional paramater in LIMIT which we can access using a comma that specifies the starting place
+SELECT *
+FROM employee_demographics
+ORDER BY first_name
+LIMIT 3, 2;
+-- This says start at position 3 and take 2 rows after that
