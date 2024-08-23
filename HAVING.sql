191a32bb536e9f63b70195fe09690659f95a5a5a
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:03:43 -0600

    Description

--- HAVING.sql
+++ HAVING.sql
@@ -1,1 +1,6 @@
 -- HAVING CLAUSE
+
+-- The HAVING clause in SQL is used to filter records after an aggregation (like COUNT(), SUM(), AVG(), etc.) has been applied. 
+-- It's similar to the WHERE clause, but WHERE filters rows before aggregation, while HAVING filters rows after aggregation.
+-- Where is going to filters rows based off columns of data
+-- Having is going to filter rows based off aggregated columns when grouped
