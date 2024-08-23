28af694e37815b1c4c44191d452a7683de7d8c52
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-23 15:49:03 -0600

    SYNTAX for Aliasing

--- ALIASING.sql
+++ ALIASING.sql
@@ -3,3 +3,7 @@
 -- In SQL, aliasing refers to the process of giving a temporary name to a table or a column
 -- It makes the query more readable or handles situations where two or more columns have the same name. 
 -- Aliasing is typically done using the 'AS' keyword.
+
+-- SYNTAX
+SELECT column_name AS alias_name
+FROM table_name
