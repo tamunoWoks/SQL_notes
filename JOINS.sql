2fc9ec39a3d7b3aa35b6172dff748d87c72cd8b7
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 14:07:44 -0600

    JOIN Syntax

--- JOINS.sql
+++ JOINS.sql
@@ -3,3 +3,9 @@
 -- In SQL, JOINS are used to combine rows from two or more tables based on a related column between them.
 -- They don't necessarily need to have the same name, as long as the data in it are the same and can be used to join the tables together
 
+--SYNTAX
+SELECT columns
+FROM table1
+JOIN table2
+ON table1.common_column = table2.common_column;
+
