12add499c2a923b5d28846beb23eb4fc1233e9a3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 17:11:00 -0600

    UNION Syntax

--- UNION.sql
+++ UNION.sql
@@ -2,3 +2,10 @@
 
 -- In SQL, the UNION operator is used to combine the result sets of two or more SELECT queries. 
 -- It removes duplicate rows between the result sets unless UNION ALL is used, which includes duplicates.
+
+-- SYNTAX
+SELECT column1, column2, ...
+FROM table1
+UNION
+SELECT column1, column2, ...
+FROM table2;
