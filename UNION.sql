f2b4c9daae00f850eb87e763b96739550183c300
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 17:36:11 -0600

    UNION example

--- UNION.sql
+++ UNION.sql
@@ -1,7 +1,7 @@
 -- UNION clause
 
--- In SQL, the UNION operator is used to combine the result sets of two or more SELECT queries. 
--- It removes duplicate rows between the result sets unless UNION ALL is used, which includes duplicates.
+-- In SQL, the UNION operator is used to combine the result sets of two or more SELECT queries. It allows you to combine the rows of data.
+-- UNION is actually shorthand for UNION DISTINCT. It removes duplicate rows between the result sets unless UNION ALL is used, which includes duplicates.
 
 -- SYNTAX
 SELECT column1, column2, ...
@@ -9,3 +9,19 @@ FROM table1
 UNION
 SELECT column1, column2, ...
 FROM table2;
+
+
+-- EXAMPLE
+SELECT first_name, last_name, 'Old Lady' AS LABEL
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Old Man'
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee'
+FROM employee_salary
+WHERE salary >= 70000
+ORDER BY first_name
+
