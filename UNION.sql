a35bfcce7d805a3d0c6c66cbfa8f8903b4e629a1
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 17:10:24 -0600

    Description

--- UNION.sql
+++ UNION.sql
@@ -1,1 +1,4 @@
+-- UNION clause
 
+-- In SQL, the UNION operator is used to combine the result sets of two or more SELECT queries. 
+-- It removes duplicate rows between the result sets unless UNION ALL is used, which includes duplicates.
