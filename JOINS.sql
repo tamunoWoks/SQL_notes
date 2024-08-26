e4a55267af092e49f2567349743ec394acce35ba
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 15:30:03 -0600

    LEFT OUTER JOIN

--- JOINS.sql
+++ JOINS.sql
@@ -9,7 +9,7 @@ FROM table1
 JOIN table2
 ON table1.common_column = table2.common_column;
 
--- There are several types of JOINS
+-- THERE ARE SEVERAL TYPES OF JOINS
 
 
 --1. INNER JOIN 
@@ -17,15 +17,23 @@ ON table1.common_column = table2.common_column;
 SELECT *
 FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sal
-  ON dem.employee.id = sal.employee.id
+  ON dem.employee.id = sal.employee.id;
 
 -- In INNER JOINS, the INNER can be omitted in the keyword and it will still work:
 SELECT *
 FROM employee_demographics AS dem
 JOIN employee_salary AS sal
-  ON dem.employee.id = sal.employee.id
+  ON dem.employee.id = sal.employee.id;
 
 
 --2. OUTER JOIN
 -- An OUTER JOIN in SQL is used to combine rows from two or more tables based on a related column between them, even if there are no matches in one of the tables. 
 -- There are three main types of OUTER JOINs:
+
+-- LEFT OUTER JOIN (LEFT JOIN)
+-- As the above title implies, it can be written as LEFT OUTER JOIN or LEFT JOI
+-- Returns all records from the left table and the matched records from the right table. If there is no match, the result is NULL on the side of the right table.
+SELECT *
+FROM employee_salary AS sal
+LEFT JOIN employee_demographic AS dem
+  ON sal.employee_id = dem.employee_id;
