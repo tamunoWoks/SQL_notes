8a4f24aa3c05a1547b24d56feebe2e2f480af7c3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 18:34:42 -0600

    COUNT()

--- AGGREGATE_FUNCTIONS.sql
+++ AGGREGATE_FUNCTIONS.sql
@@ -2,3 +2,10 @@
 
 -- Aggregate functions in SQL are used to perform calculations on a set of values and return a single value. 
 -- These functions are commonly used with the GROUP BY clause to group rows sharing a property and calculate aggregate values for each group.
+
+--EXAMPLES OF AGGREGATE FUNCTIONS
+
+-- 1. COUNT()
+-- Returns the number of rows that match a specified condition.
+SELECT COUNT(*) AS total_employees
+FROM employee_demographic
