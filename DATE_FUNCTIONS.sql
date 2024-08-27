f6635310110fcde675613282d5dfd60cacdf6bdc
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-27 17:15:35 -0600

    CURRENT_DATE()

--- DATE_FUNCTIONS.sql
+++ DATE_FUNCTIONS.sql
@@ -2,3 +2,10 @@
 
 -- Date functions in SQL are used to perform operations on date and time values. 
 -- Here are some commonly used date functions in SQL:
+
+
+-- 1. CURRENT_DATE()
+--Returns the current date.
+SELECT CURRENT_DATE() AS today_date;
+
+
