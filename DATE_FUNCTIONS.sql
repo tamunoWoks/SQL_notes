dc92b5b1a0d945a3859a50e3f3cbd9243e81a6d2
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-27 17:50:23 -0600

    EXTRACT()

--- DATE_FUNCTIONS.sql
+++ DATE_FUNCTIONS.sql
@@ -30,17 +30,46 @@ SELECT DATE('2024-08-27 14:23:55') AS date_part;
 SELECT DATE_ADD('2024-08-27', INTERVAL 7 DAY) AS next_week;
 -- Output: 2024-09-03
 
+
 -- 6. DATE_SUB()
 -- Subtracts a time/date interval from a date
 SELECT DATE_SUB('2024-08-27', INTERVAL 7 DAY) AS last_week;
 -- Output: 2024-08-20
+
 
 -- 7. DATEDIFF()
 -- Returns the difference in days between two dates.
 SELECT DATEDIFF('2024-08-27', '2024-08-20') AS days_difference;
 -- Output: 7
 
-8. DATE_FORMAT()
+
+-- 8. DATE_FORMAT()
 -- Formats a date according to the specified format.
 SELECT DATE_FORMAT('2024-08-27', '%W, %M %d, %Y') AS formatted_date;
 -- Output: Tuesday, August 27, 2024
+
+
+-- 9. EXTRACT()
+-- Extracts parts of a date (like year, month, day, etc.).
+SELECT EXTRACT(YEAR FROM '2024-08-27') AS year_part,
+       EXTRACT(MONTH FROM '2024-08-27') AS month_part,
+       EXTRACT(DAY FROM '2024-08-27') AS day_part;
+-- Output: 2024       8           27
+
+
+-- Output:
+
+
+-- Output:
+
+
+-- Output:
+
+
+-- Output:
+
+
+-- Output:
+
+
+-- Output:
