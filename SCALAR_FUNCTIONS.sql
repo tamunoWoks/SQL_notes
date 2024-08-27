4d3a5de8f5373710126b7847395827b37a801105
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 19:24:11 -0600

    NOW() / GETDATE()

--- SCALAR_FUNCTIONS.sql
+++ SCALAR_FUNCTIONS.sql
@@ -9,10 +9,12 @@
 SELECT UPPER('hello world') AS UppercaseString;
 -- Output: 'HELLO WORLD'
 
+
 -- 2. LOWER()
 -- Converts a string to lowercase.
 SELECT LOWER('HELLO WORLD') AS LowercaseString;
 -- Output: 'hello world'
+
 
 -- 3. LEN() / LENGTH()
 -- Returns the length of a string. The function name can vary by SQL database.
@@ -22,6 +24,7 @@ SELECT LEN('Hello') AS StringLength;  -- SQL Server
 SELECT LENGTH('Hello') AS StringLength;  -- MySQL, PostgreSQL
 -- Output: 5
 
+
 -- 4. SUBSTRING() / SUBSTR()
 -- Extracts a substring from a string.
 SELECT SUBSTRING('Hello World', 1, 5) AS Substring;  -- SQL Server
@@ -29,6 +32,7 @@ SELECT SUBSTRING('Hello World', 1, 5) AS Substring;  -- SQL Server
 
 SELECT SUBSTR('Hello World', 1, 5) AS Substring;  -- Oracle, MySQL
 -- Output: 'Hello'
+
 
 -- 5. CONCAT()
 -- Concatenates two or more strings.
@@ -38,7 +42,17 @@ SELECT CONCAT('Hello', ' ', 'World') AS ConcatenatedString;  -- MySQL, PostgreSQ
 SELECT 'Hello' + ' ' + 'World' AS ConcatenatedString;  -- SQL Server
 -- Output: 'Hello World'
 
--- ROUND()
+
+-- 6. ROUND()
 -- Rounds a numeric value to a specified number of decimal places.
 SELECT ROUND(123.4567, 2) AS RoundedNumber;
 -- Output: 123.46
+
+
+-- 7. NOW() / GETDATE()
+-- Returns the current date and time.
+SELECT NOW() AS CurrentDateTime;  -- PostgreSQL, MySQL
+-- Output: '2024-08-26 12:34:56'
+
+SELECT GETDATE() AS CurrentDateTime;  -- SQL Server
+-- Output: '2024-08-26 12:34:56'
