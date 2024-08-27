dbf3e8967f0b56bc596ced5f2d0e9cac6502f663
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-27 14:09:09 -0600

    CHARINDEX() / POSITION()

--- STRING_FUNCTIONS.sql
+++ STRING_FUNCTIONS.sql
@@ -26,3 +26,12 @@ SELECT RTRIM('Hello World   ') AS RTrimmedString;
 -- Replaces all occurrences of a specified substring with another substring.
 SELECT REPLACE('Hello World', 'World', 'SQL') AS ReplacedString;
 -- Output: 'Hello SQL'
+
+
+-- 5. CHARINDEX() / POSITION()
+-- Finds the position of a substring within a string.
+SELECT CHARINDEX('World', 'Hello World') AS Position;  -- SQL Server
+-- Output: 7
+
+SELECT POSITION('World' IN 'Hello World') AS Position;  -- PostgreSQL
+-- Output: 7
