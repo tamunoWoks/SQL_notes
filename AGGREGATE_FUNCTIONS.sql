e8b21d084137df43e0e1186acc4d4598cf2c41ca
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 18:14:19 -0600

    Description

--- AGGREGATE_FUNCTIONS.sql
+++ AGGREGATE_FUNCTIONS.sql
@@ -1,1 +1,4 @@
+-- AGGREGATE FUNCTIONS
 
+-- Aggregate functions in SQL are used to perform calculations on a set of values and return a single value. 
+-- These functions are commonly used with the GROUP BY clause to group rows sharing a property and calculate aggregate values for each group.
