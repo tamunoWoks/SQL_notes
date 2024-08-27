76cf0e71ee53650682c89556558f796d87eb438c
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-26 19:16:02 -0600

    UPPER()

--- SCALAR_FUNCTIONS.sql
+++ SCALAR_FUNCTIONS.sql
@@ -3,3 +3,8 @@
 -- Scalar functions in SQL are functions that operate on individual values and return a single value. 
 
 -- EXAMPLES OF SCALAR FUNCTIONS
+
+-- 1. UPPER()
+-- Converts a string to uppercase.
+SELECT UPPER('hello world') AS UppercaseString;
+-- Output: 'HELLO WORLD'
