0adf0199e0bbb69e5a618854eb59766619fabcd3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-27 14:01:16 -0600

    Description

--- STRING_FUNCTIONS.sql
+++ STRING_FUNCTIONS.sql
@@ -1,1 +1,4 @@
+-- STRING MANIPULATION FUNCTIONS
 
+-- String manipulation functions are essential for processing and transforming text data in SQL. 
+-- Here’s a list of common string manipulation functions along with examples for various SQL databases:
