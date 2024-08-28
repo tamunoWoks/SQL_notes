b8fa3a2f5eade0a010c533052d24ae2025be8525
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 17:42:42 -0600

    CTE Description

--- CTE.sql
+++ CTE.sql
@@ -1,1 +1,4 @@
+-- CTE (Common Table Expressions)
 
+-- Common Table Expressions (CTEs) are used to define temporary result sets that can be referenced within a SELECT, INSERT, UPDATE, or DELETE statement. 
+-- CTEs can be particularly useful for simplifying complex queries, making them more readable and easier to maintain.
