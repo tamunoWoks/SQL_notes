5ceb4e8448f6a67053a844a69c417712d3d5515f
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 15:23:35 -0600

    Description

--- Subqueries.sql
+++ Subqueries.sql
@@ -1,1 +1,4 @@
+-- SUBQUERIES
 
+-- Subqueries in SQL are queries nested inside another query. 
+-- They are often used to perform operations that require multiple steps, like filtering, calculating, or combining data across multiple tables or rows. 
