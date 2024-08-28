f32fec28068d1d8abbf4e58c38897639e1788a31
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 13:35:53 -0600

    CASE Syntax

--- CASE.sql
+++ CASE.sql
@@ -2,3 +2,14 @@
 
 -- A CASE statement in SQL is used to create conditional logic within queries. 
 -- It allows you to perform if-else-like operations within your SQL statements.  
+
+-- BASIC SYNTAX
+SELECT
+    CASE
+        WHEN condition1 THEN result1
+        WHEN condition2 THEN result2
+        ...
+        ELSE default_result
+    END AS new_column_name
+FROM
+    table_name;
