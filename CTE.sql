3dd16d46013f933238a62ed9a8bb07d349b91d88
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 17:51:10 -0600

    Using multiple CTEs

--- CTE.sql
+++ CTE.sql
@@ -51,3 +51,22 @@ SELECT gender, ROUND(AVG(`SUM(salary)`/`COUNT(salary)`),2)
 FROM CTE_Example
 GROUP BY gender;
 
+
+-- 3. USING MULTIPLE CTEs
+WITH CTE_Example AS 
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics dem
+WHERE birth_date > '1985-01-01'
+), -- just have to separate by using a comma
+CTE_Example2 AS 
+(
+SELECT employee_id, salary
+FROM parks_and_recreation.employee_salary
+WHERE salary >= 50000
+)
+-- Now if we change this a bit, we can join these two CTEs together
+SELECT *
+FROM CTE_Example cte1
+LEFT JOIN CTE_Example2 cte2
+	ON cte1. employee_id = cte2. employee_id;
