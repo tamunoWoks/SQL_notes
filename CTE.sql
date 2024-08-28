a10cd7491e126084cd474c8628bb309fcb49bf3f
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 17:44:11 -0600

    CTE Syntax

--- CTE.sql
+++ CTE.sql
@@ -2,3 +2,12 @@
 
 -- Common Table Expressions (CTEs) are used to define temporary result sets that can be referenced within a SELECT, INSERT, UPDATE, or DELETE statement. 
 -- CTEs can be particularly useful for simplifying complex queries, making them more readable and easier to maintain.
+
+-- BASIC CTE SYNTAX
+WITH cte_name AS (
+    -- Your complex query here
+    SELECT column1, column2
+    FROM your_table
+    WHERE some_condition
+)
+SELECT * FROM cte_name;
