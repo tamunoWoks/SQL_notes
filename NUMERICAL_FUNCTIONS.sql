40f8bebd8b81e4c475ffa11ad9a35c1c548d2209
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 13:06:45 -0600

    ABS(x)

--- NUMERICAL_FUNCTIONS.sql
+++ NUMERICAL_FUNCTIONS.sql
@@ -4,3 +4,9 @@
 -- These functions help with calculations, rounding, absolute value, and more. 
 
 -- Here’s an overview of some commonly used numerical functions in SQL:
+
+
+-- 1. ABS(x)
+-- Returns the absolute value of a number x.
+SELECT ABS(-10) AS AbsoluteValue;
+-- Result: 10
