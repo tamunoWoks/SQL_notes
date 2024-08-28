02c5704769258d6d83653e9c14ca161fbb50dd17
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 15:06:13 -0600

    Description

--- BETWEEN.sql
+++ BETWEEN.sql
@@ -1,1 +1,5 @@
+-- BETWEEN CLAUSE
 
+-- The BETWEEN operator in SQL is used to filter the result set within a certain range. 
+-- The range can be numeric, text, or date values. 
+-- The BETWEEN operator includes the endpoints of the range.
