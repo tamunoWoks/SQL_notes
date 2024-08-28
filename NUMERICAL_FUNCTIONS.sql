899337bb9de9b12aa5f785d9df173ab776d9de57
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 13:05:49 -0600

    Description

--- NUMERICAL_FUNCTIONS.sql
+++ NUMERICAL_FUNCTIONS.sql
@@ -1,1 +1,6 @@
+-- NUMERICAL FUNCTIONS
 
+-- In SQL, numerical functions are used to perform operations on numeric data types such as INT, FLOAT, DECIMAL, etc. 
+-- These functions help with calculations, rounding, absolute value, and more. 
+
+-- Here’s an overview of some commonly used numerical functions in SQL:
