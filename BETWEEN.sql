dd230d90f4c0ebf1f7b9cc4c1e9e66ceb55a5982
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 15:06:42 -0600

    BETWEEN Syntax

--- BETWEEN.sql
+++ BETWEEN.sql
@@ -3,3 +3,8 @@
 -- The BETWEEN operator in SQL is used to filter the result set within a certain range. 
 -- The range can be numeric, text, or date values. 
 -- The BETWEEN operator includes the endpoints of the range.
+
+-- SYNTAX
+SELECT column_name(s)
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;
