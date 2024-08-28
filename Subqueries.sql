975c7cab68e630e5a1eec6ce108e57f182dd86ac
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 15:25:51 -0600

    Example1

--- Subqueries.sql
+++ Subqueries.sql
@@ -2,3 +2,15 @@
 
 -- Subqueries in SQL are queries nested inside another query. 
 -- They are often used to perform operations that require multiple steps, like filtering, calculating, or combining data across multiple tables or rows. 
+
+
+-- EXAMPLES
+
+-- 1. Let's look at employees who actually work in the Parks and Rec Department, we could join tables together or we could use a subquery:
+SELECT *
+FROM employee_demographics
+WHERE employee_id IN 
+			(SELECT employee_id
+				FROM employee_salary
+                WHERE dept_id = 1);            
+-- We are using that subquery in the where statement and if we just highlight the subquery and run it it's basically a list we are selecting from in the outer query
