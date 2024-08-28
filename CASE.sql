c0291630a185a4b06d0260f614be3bd32fa18042
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-28 13:35:05 -0600

    Descr

--- CASE.sql
+++ CASE.sql
@@ -1,1 +1,4 @@
+-- CASE STATEMENT
 
+-- A CASE statement in SQL is used to create conditional logic within queries. 
+-- It allows you to perform if-else-like operations within your SQL statements.  
