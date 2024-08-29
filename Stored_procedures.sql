afffbb306dea09b827668f3a63d677794f355d77
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 11:15:25 -0600

    Creating a Stored Procedure

--- Stored_procedures.sql
+++ Stored_procedures.sql
@@ -2,3 +2,13 @@
 
 -- Stored procedures in SQL are a powerful tool that allow you to encapsulate SQL code in a reusable, maintainable, and secure way. 
 -- They are a collection of SQL statements and logic that are stored on the database server and can be executed by calling the procedure.
+
+
+-- Creating a Stored Procedure
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary >= 60000;
+-- If we want to call it and use it we can call it by saying:
+CALL large_salaries();
+
