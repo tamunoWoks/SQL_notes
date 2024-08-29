6b767e4831b3dd32756a342f6b4ea1ff08ec7116
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 16:41:11 -0600

    DROP_DATABASE Syntax

--- DROP_DATABASE.sql
+++ DROP_DATABASE.sql
@@ -1,1 +1,8 @@
+-- DROP DATABASE Statement
 
+-- The DROP DATABASE statement in SQL is used to delete an entire database, including all of its tables, data, views, indexes, and other objects contained within it. 
+-- This action is irreversible, meaning that once the database is dropped, all of its contents are permanently removed and cannot be recovered.
+
+-- Syntax:
+DROP DATABASE database_name;
+-- database_name: The name of the database you want to drop.
