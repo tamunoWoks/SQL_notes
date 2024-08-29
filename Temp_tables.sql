3a86f47ae63756ea72c98c84341b41b4be91a5e3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 10:19:42 -0600

    Temp_tables description

--- Temp_tables.sql
+++ Temp_tables.sql
@@ -1,1 +1,4 @@
+-- TEMP TABLES
 
+-- Temporary tables in SQL are special types of tables that are created and used to store intermediate results temporarily within a session or a transaction. 
+-- They are useful for breaking down complex queries into simpler steps, storing intermediate results, and improving performance in scenarios where data needs to be reused within a session.
