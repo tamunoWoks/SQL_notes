fc9406e9f56a5b69327c7067790186bffcbccdbc
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:17:42 -0600

    Basic Syntax

--- DROP_TABLE.sql
+++ DROP_TABLE.sql
@@ -1,1 +1,8 @@
+-- DROP TABLE Statement
 
+-- The DROP TABLE statement in SQL is used to delete an existing table in a database. 
+-- When you use this command, the table and all the data it contains are permanently removed, and this action cannot be undone. 
+-- It also removes all the indexes, triggers, constraints, and permission specifications associated with the table.
+
+-- Basic Syntax:
+DROP TABLE table_name;
