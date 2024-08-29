6721f5f0e3f81c17f50df067cc6be77b53f4bd6a
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:39:14 -0600

    Create Constraints

--- CONSTRAINTS.sql
+++ CONSTRAINTS.sql
@@ -1,3 +1,15 @@
 -- CONSTRAINTS
 
 -- SQL constraints are used to specify rules for data in a table.
+
+
+-- Create Constraints
+-- Constraints can be specified when the table is created with the CREATE TABLE statement, or after the table is created with the ALTER TABLE statement.
+
+--Syntax:
+CREATE TABLE table_name (
+    column1 datatype constraint,
+    column2 datatype constraint,
+    column3 datatype constraint,
+    ....
+);
