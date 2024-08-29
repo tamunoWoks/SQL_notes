fabb09e126b55cabc2984c87e24f49b26ad7cbed
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 10:27:04 -0600

    Build temp table by inserting data into it

--- Temp_tables.sql
+++ Temp_tables.sql
@@ -21,3 +21,13 @@ FROM temp_table;
 INSERT INTO temp_table
 VALUES ('Tammy','Wokoma','Alchemy of Souls');
 -- now when we run it and execute it again we have our data
+
+
+-- 2. Build temp table by inserting data into it
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+-- if we run this query we get our output
+SELECT *
+FROM salary_over_50k;
