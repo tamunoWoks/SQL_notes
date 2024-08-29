16e4fa25a0038a2fcbca3109e6da04f6e2f8b97d
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:50:46 -0600

    Description

--- Check_constraint.sql
+++ Check_constraint.sql
@@ -1,1 +1,5 @@
+-- MySQL CHECK Constraint
 
+--The CHECK constraint is used to limit the value range that can be placed in a column.
+-- If you define a CHECK constraint on a column it will allow only certain values for this column.
+-- If you define a CHECK constraint on a table it can limit the values in certain columns based on values in other columns in the row.
