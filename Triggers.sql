6ed8dc596fd27ca7bc5f388c1c4d1ced1dfdf2cb
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 11:44:33 -0600

    Triggers description

--- Triggers.sql
+++ Triggers.sql
@@ -1,1 +1,5 @@
+-- TRIGGERS
 
+-- In SQL, triggers are special types of stored procedures that are automatically executed or "triggered" when certain events occur in a database. 
+-- Triggers can be set to run before or after an insert, update, or delete operation on a table. 
+-- They are commonly used to enforce business rules, maintain audit trails, or synchronize related tables.
