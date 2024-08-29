6afe42ace681a53c849dbbb8f0d8248bef5f15a7
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:00:15 -0600

    Syntax

--- CREATE_TABLE.sql
+++ CREATE_TABLE.sql
@@ -1,1 +1,11 @@
+-- CREATE TABLE Statement
 
+-- The CREATE TABLE statement is used to create a new table in a database.
+
+-- Syntax:
+CREATE TABLE table_name (
+    column1 datatype constraints,
+    column2 datatype constraints,
+    column3 datatype constraints,
+    ...
+);
