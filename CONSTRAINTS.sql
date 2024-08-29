065b7aa5146c17ebde486a62d482e78dc3958474
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:41:32 -0600

    Add notes

--- CONSTRAINTS.sql
+++ CONSTRAINTS.sql
@@ -13,3 +13,21 @@ CREATE TABLE table_name (
     column3 datatype constraint,
     ....
 );
+
+
+-- SQL constraints are used to specify rules for the data in a table.
+
+-- Constraints are used to limit the type of data that can go into a table. 
+-- This ensures the accuracy and reliability of the data in the table. 
+-- If there is any violation between the constraint and the data action, the action is aborted.
+
+-- Constraints can be column level or table level. Column level constraints apply to a column, and table level constraints apply to the whole table.
+
+-- THE FOLLOWING CONSTRAINTS ARE COMMONLY USED IN SQL:
+-- NOT NULL - Ensures that a column cannot have a NULL value
+-- UNIQUE - Ensures that all values in a column are different
+-- PRIMARY KEY - A combination of a NOT NULL and UNIQUE. Uniquely identifies each row in a table
+-- FOREIGN KEY - Prevents actions that would destroy links between tables
+-- CHECK - Ensures that the values in a column satisfies a specific condition
+-- DEFAULT - Sets a default value for a column if no value is specified
+-- CREATE INDEX - Used to create and retrieve data from the database very quickly
