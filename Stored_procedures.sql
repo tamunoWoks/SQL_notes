0d8f67202be675a36546d13fe2b43267e4b83f4f
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 11:12:56 -0600

    Stored_procedures description

--- Stored_procedures.sql
+++ Stored_procedures.sql
@@ -1,1 +1,4 @@
+-- STORED PROCEDURES
 
+-- Stored procedures in SQL are a powerful tool that allow you to encapsulate SQL code in a reusable, maintainable, and secure way. 
+-- They are a collection of SQL statements and logic that are stored on the database server and can be executed by calling the procedure.
