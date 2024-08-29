725a6f2d127146db4b0dc515cbc39c4839ec6a24
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 17:23:13 -0600

    Description

--- ALTER_TABLE.sql
+++ ALTER_TABLE.sql
@@ -1,1 +1,4 @@
+-- ALTER TABLE Statement
 
+-- The ALTER TABLE statement in SQL is used to modify the structure of an existing table. 
+-- This can include adding or dropping columns, changing the data type of a column, renaming columns or the table itself, and adding or dropping constraints on an existing table.
