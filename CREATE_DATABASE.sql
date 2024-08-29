09729f9bf29a3f6ac358afdc0962a347d73ddd2d
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 16:27:11 -0600

    CREATE_DATABASE example

--- CREATE_DATABASE.sql
+++ CREATE_DATABASE.sql
@@ -3,3 +3,8 @@
 -- The CREATE DATABASE statement in MySQL is used to create a new database. 
 -- The basic syntax is:
 CREATE DATABASE database_name;
+
+
+-- Example:
+-- Suppose you want to create a database named company_db. The SQL statement would be:
+CREATE DATABASE company_db;
