544e9daabdd1cd88be336a8b22376815fccb3711
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-08-29 11:58:43 -0600

    Events description

--- Events.sql
+++ Events.sql
@@ -1,1 +1,5 @@
+-- EVENTS
 
+-- In SQL, an EVENT is a task that is scheduled to run at a specific time or interval. 
+-- It is similar to a scheduled job in other database management systems. 
+-- Events can be used to automate repetitive tasks, such as database maintenance, data archiving, or generating reports.
